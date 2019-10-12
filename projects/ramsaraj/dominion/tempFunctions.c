939482944a6f0c688d0eec53da2bd919c72473c2
James Ramsaran <james.j.ramsaran@gmail.com>
2019-10-12 15:53:13 -0400

    added playTribute code to tempFunctions
    - minor syntax fixes
    - added refactored Tribute code

--- projects/ramsaraj/dominion/tempFunctions.c
+++ projects/ramsaraj/dominion/tempFunctions.c
@@ -1,3 +1,7 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "interface.h"
+
 int playBaron(struct gameState* state, int choice, int currentPlayer)
 {
 	state->numBuys++; //increase the number of buys for the current player
@@ -8,10 +12,10 @@ int playBaron(struct gameState* state, int choice, int currentPlayer)
 			if ( (p = cardInHand(estate, state, currentPlayer)) != -1 ) {
 				state->coins += 4;
 				state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][p];
-				state->discardcount[currentPlayer]++;
+				state->discardCount[currentPlayer]++;
 				//Remove the estate card from the player's hand
 				for (; p < state->handCount[currentPlayer]; p++) {
-					state->hand[currentPLayer][p] = state->hand[currentPlayer][p+1];
+					state->hand[currentPlayer][p] = state->hand[currentPlayer][p+1];
 					
 				}
 				state->hand[currentPlayer][state->handCount[currentPlayer]] = -1;
@@ -49,7 +53,7 @@ int playBaron(struct gameState* state, int choice, int currentPlayer)
 }
  
 					
-int cardInHand(CARD targetCard, struct gameState *state, int currentPlayer) {
+int cardInHand(enum CARD targetCard, struct gameState *state, int currentPlayer) {
 	for(int pos = 0; pos < state->handCount[currentPlayer]; pos++) {
 		if ( state->hand[currentPlayer][pos] == targetCard ) {
 			return pos;
@@ -57,4 +61,75 @@ int cardInHand(CARD targetCard, struct gameState *state, int currentPlayer) {
 	} 
 	
 	return -1;
+}
+
+int playTribute(int currentPlayer, int nextPlayer, struct gameState *state) {
+	//Create array to store the cards revealed by the next player:
+	int tributeRevealedCards[2] = {-1, -1};
+	
+	//Determine whether the next player only has at least 2 cards in their combined hand & deck.
+	//If not, the next player either reveals their single card or doesn't reveal anything:  
+	if ((state->discardCount[nextPlayer] + state->deckCount[nextPlayer]) <= 1) {
+        if (state->deckCount[nextPlayer] > 0) {
+            tributeRevealedCards[0] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
+            state->deckCount[nextPlayer]--;
+        }
+        else if (state->discardCount[nextPlayer] > 0) {
+            tributeRevealedCards[0] = state->discard[nextPlayer][state->discardCount[nextPlayer]-1];
+            state->discardCount[nextPlayer]--;
+        }
+        else {
+            //No Card to Reveal
+            if (DEBUG) {
+                printf("No cards to reveal\n");
+            }
+        }
+	}
+
+	//If the next player does have at least 2 cards:
+	else {
+		//Check if deck is empty and if so, add 
+        if (state->deckCount[nextPlayer] == 0) {
+            for (int i = 0; i < state->discardCount[nextPlayer]; i++) {
+                state->deck[nextPlayer][i] = state->discard[nextPlayer][i];//Move to deck
+                state->deckCount[nextPlayer]++;
+                state->discard[nextPlayer][i] = -1;
+                state->discardCount[nextPlayer]--;
+            }
+
+            shuffle(nextPlayer,state);//Shuffle the deck
+        }
+        tributeRevealedCards[0] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
+        state->deck[nextPlayer][state->deckCount[nextPlayer]--] = -1;
+        state->deckCount[nextPlayer]--;
+        tributeRevealedCards[1] = state->deck[nextPlayer][state->deckCount[nextPlayer]-1];
+        state->deck[nextPlayer][state->deckCount[nextPlayer]--] = -1;
+        state->deckCount[nextPlayer]--;
+    }
+
+	//Check if the two cards revealed are duplicates:
+	if (tributeRevealedCards[0] == tributeRevealedCards[1]) { //If we have a duplicate card, just drop one
+            state->playedCards[state->playedCardCount] = tributeRevealedCards[1];
+            state->playedCardCount++;
+            tributeRevealedCards[1] = -1;
+        }
+
+	//Check the cards in the tribute card array and give appropiate bonuses for each:
+	for (int i = 0; i <= 2; i ++) {
+			//This series of if statements ensures that a duplicate card changed to `-1` does not give any bonuses:
+            if (tributeRevealedCards[i] >= copper && tributeRevealedCards[i] <= gold) { //Treasure cards
+                state->coins += 2;
+            }
+
+            if ((tributeRevealedCards[i] >= estate && tributeRevealedCards[i] <= province) || tributeRevealedCards[i] == gardens) { //Victory Card Found
+                drawCard(currentPlayer, state);
+                drawCard(currentPlayer, state);
+            }
+            if ((tributeRevealedCards[i] >= adventurer && tributeRevealedCards[i] <= treasure_map) && tributeRevealedCards[i] != gardens) { //Action Card Found
+                state->numActions = state->numActions + 2;
+			}
+        }
+
+    return 0;
+
 }
