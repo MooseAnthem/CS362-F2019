1241f9c3ac46e8efa2e8100f2d3161420895eba5
James Ramsaran <james.j.ramsaran@gmail.com>
2019-10-13 01:21:57 -0400

    put refactored functions and prototypes into source
    - put refactored functions into dominion.c
    - put refactored function prototypes into dominion.h
    - replaced switch code with calls to the new refactored functions

--- projects/ramsaraj/dominion/dominion.h
+++ projects/ramsaraj/dominion/dominion.h
@@ -128,4 +128,14 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int playBaron(int choice, int currentPlayer, struct gameState* state);
+
+int cardInHand(enum CARD targetCard, struct gameState *state, int currentPlayer);
+
+int playTribute(int currentPlayer, int nextPlayer, struct gameState *state);
+
+int playAmbassador(int handPos, int currentPlayer, int cardToDiscard, int quantityToDiscard, struct gameState* state);
+
+int playAmbassador(int handPos, int currentPlayer, int cardToDiscard, int quantityToDiscard, struct gameState* state);
+
 #endif
