166140bd4130cc131135ad6adf6cec7044a835fc
Luke <burrisl@oregonstate.edu>
2019-12-02 17:43:35 -0500

    Update unittest1.c

--- projects/FinalProject-Bugs/dominion/unittest1.c
+++ projects/FinalProject-Bugs/dominion/unittest1.c
@@ -57,7 +57,7 @@ int main () {
 
     // Call the function being tested;
     // Signature: cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
-    cardEffect(mine, choice1, choice2, 0, state, handPos, 0);
+    //cardEffect(mine, choice1, choice2, 0, state, handPos, 0);
 
     // Do tests
     checkTrue(state.playedCardCount, 1, "Played Card Count Equals 1");
