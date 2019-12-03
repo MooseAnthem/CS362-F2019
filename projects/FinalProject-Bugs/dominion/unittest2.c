47a0583c8e4abcfb8d16e35f18b694063c58ef68
Luke <burrisl@oregonstate.edu>
2019-12-02 19:19:07 -0500

    Update unittest2.c

--- projects/FinalProject-Bugs/dominion/unittest2.c
+++ projects/FinalProject-Bugs/dominion/unittest2.c
@@ -29,7 +29,7 @@ void checkTrue(int x, int y, char* desc) {
 int main () {
 	// Set up variables for tests
     int choice1 = 1;
-    int choice2 = gold;
+    int choice2 = silver;
     int handPos = 0;
     int numPlayers = 2;
     int currentPlayer = 0;
