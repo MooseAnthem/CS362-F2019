32ab3818fac834fc3d9df6a5c395e5a11d7cfa43
Luke <burrisl@oregonstate.edu>
2019-12-02 19:59:23 -0500

    Update unittest4.c

--- projects/FinalProject-Bugs/dominion/unittest4.c
+++ projects/FinalProject-Bugs/dominion/unittest4.c
@@ -30,6 +30,13 @@ int main () {
 	// Set up variables for tests
     int numPlayers = 2;
     int currentPlayer = 0;
+
+    // Print out test title
+    printFormatted("UNITTEST4 - isGameOver iterates over all cards");
+
+    /*********************************************************************
+    *********************** SUBTEST 1 ************************************
+    *********************************************************************/
     
     int k[10] = {adventurer, council_room, feast, gardens, sea_hag,
         remodel, smithy, village, baron, great_hall};
@@ -43,14 +50,37 @@ int main () {
     // Set the supply counts of 3 cards to 0
     state.supplyCount[adventurer] = 0;
     state.supplyCount[council_room] = 0;
-    state.supplyCount[baron] = 0;
-
-    // Print out test title
-    printFormatted("UNITTEST4 - isGameOver iterates over all cards");
+    state.supplyCount[sea_hag] = 0;
 
     // Call the function being tested
     // Signature: int isGameOver(struct gameState *state)
     // Do tests
+    printFormatted("SUBTEST1 - sea_hag's supplyCount set to 0");
+    checkTrue(isGameOver(&state), 1, "isGameOver Returns 1");
+
+
+    /*********************************************************************
+    *********************** SUBTEST 2 ************************************
+    *********************************************************************/
+
+    int k[10] = {adventurer, council_room, feast, gardens, sea_hag,
+        remodel, smithy, village, baron, great_hall};
+
+    struct gameState state;
+
+    // Initialize the game struct
+    memset(&state, 23, sizeof(struct gameState));
+    initializeGame(numPlayers, k, 618, &state);
+
+    // Set the supply counts of 3 cards to 0
+    state.supplyCount[adventurer] = 0;
+    state.supplyCount[council_room] = 0;
+    state.supplyCount[treasure_map] = 0;
+
+    // Call the function being tested
+    // Signature: int isGameOver(struct gameState *state)
+    // Do tests
+    printFormatted("SUBTEST1 - treasure_map's supplyCount set to 0");
     checkTrue(isGameOver(&state), 1, "isGameOver Returns 1");
 
     return 0;
